936aadc744b1bee2eef6bed77f70e2a1eeba234a
Kishore Ch <kishu.559@gmail.com>
2023-08-19 11:20:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import EmpListing from "./Components/EmpListing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD-JSON Operations</h1>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<EmpListing/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
